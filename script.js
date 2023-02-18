9cdf1d3ac794be7545f97c0d22d50ef217b57a99
[Nazmul Kabir] <[nazmulkabirtushar@gmail.com]>
2023-02-19 03:18:10 +0600

    header section done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.addEventListener('scroll', () => {
-    document.querySelector('nav'). classList.toggle('window-scrolled'), window.scrollY > 0;
+    document.querySelector('nav'). classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+//contact button (circular text button)
+const textButtons = document.querySelectorAll('.contact-btn');
+
+textButtons.forEach(textButton =>{
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
