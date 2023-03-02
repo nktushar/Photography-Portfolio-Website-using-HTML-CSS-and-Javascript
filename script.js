8bb23e6e1a8bf9f7254a093bcf0fc01fd7ad8143
[Nazmul Kabir] <[nazmulkabirtushar@gmail.com]>
2023-03-03 05:50:33 +0600

    design complete accept responsiveness

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ textButtons.forEach(textButton =>{
 
     text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
+
+
+
+
+// initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        599:{
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1025: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+  });
