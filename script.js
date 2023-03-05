b1595c5d18b457e7e36b6be296a8cf3c6fd981d8
[Nazmul Kabir] <[nazmulkabirtushar@gmail.com]>
2023-03-06 03:33:39 +0600

    responsive nav done

--- script.js
+++ script.js
@@ -33,3 +33,28 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
   });
+
+
+  const nav = document.querySelector('.nav-links');
+  const openNavBtn = document.querySelector('#nav-toggle-open');
+  const closeNavBtn = document.querySelector('#nav-toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    closeNavBtn.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+  }
+  closeNavBtn.addEventListener('click', closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
+
+  
